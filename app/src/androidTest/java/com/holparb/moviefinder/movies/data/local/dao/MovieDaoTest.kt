ca554bd87f3620f16f661a2ab303724263aa08de
Balazs Holpar <balazs02.holpar@gmail.com>
2024-09-30 17:28:41 +0200

    MovieDao tests

--- app/src/androidTest/java/com/holparb/moviefinder/movies/data/local/dao/MovieDaoTest.kt
+++ app/src/androidTest/java/com/holparb/moviefinder/movies/data/local/dao/MovieDaoTest.kt
@@ -25,6 +25,59 @@ class MovieDaoTest {
 
     private lateinit var movieDao: MovieDao
     private lateinit var movieDatabase: MovieDatabase
+    private val movies = listOf(
+        MovieEntity(
+            id = 1,
+            title = "title1",
+            overview = "overview",
+            releaseDate = "2023-03-12",
+            posterPath = "posterPath",
+            backdropPath = "backdropPath",
+            genreIds = listOf(1,2,3),
+        ),
+        MovieEntity(
+            id = 2,
+            title = "title2",
+            overview = "overview",
+            releaseDate = "2024-09-25",
+            posterPath = "posterPath",
+            backdropPath = "backdropPath",
+            genreIds = listOf(1,2,3),
+        ),
+        MovieEntity(
+            id = 3,
+            title = "title3",
+            overview = "overview",
+            releaseDate = "2000-01-01",
+            posterPath = "posterPath",
+            backdropPath = "backdropPath",
+            genreIds = listOf(1,2,3),
+        )
+    )
+
+    private fun createMovieListOfSpecifiedSite(size: Int, isPopular: Boolean = false, isTopRated: Boolean = false, isUpcoming: Boolean = false): List<MovieEntity> {
+        val movies = mutableListOf<MovieEntity>()
+
+        for (i in 1..size ) {
+            val movie = MovieEntity(
+                id = i,
+                title = "title",
+                overview = "overview",
+                releaseDate = "2024-09-25",
+                posterPath = "posterPath",
+                backdropPath = "backdropPath",
+                genreIds = listOf(1,2,3),
+                isPopular = isPopular,
+                isTopRated = isTopRated,
+                isUpcoming = isUpcoming
+            )
+            movies.add(movie)
+        }
+
+        println("Generated size: $movies")
+
+        return movies
+    }
 
     @Before
     fun setup() {
@@ -44,45 +97,68 @@ class MovieDaoTest {
 
     @Test
     @Throws(Exception::class)
-    fun writeMoviesAndGetList() = runTest {
-        val movies = listOf(
-            MovieEntity(
-                id = 1,
-                title = "title1",
-                overview = "overview",
-                releaseDate = "2023-03-12",
-                posterPath = "posterPath",
-                backdropPath = "backdropPath",
-                genreIds = listOf(1,2,3),
-                isPopular = true
-            ),
-            MovieEntity(
-                id = 2,
-                title = "title2",
-                overview = "overview",
-                releaseDate = "2024-09-25",
-                posterPath = "posterPath",
-                backdropPath = "backdropPath",
-                genreIds = listOf(1,2,3),
-                isPopular = true
-            ),
-            MovieEntity(
-                id = 3,
-                title = "title3",
-                overview = "overview",
-                releaseDate = "2000-01-01",
-                posterPath = "posterPath",
-                backdropPath = "backdropPath",
-                genreIds = listOf(1,2,3),
-                isPopular = true
-            )
-        )
+    fun get_popular_movies_for_home_screen_successfully() = runTest {
+        val popularMovies = movies.map {
+            it.copy(isPopular = true)
+        }
 
-        movieDao.upsertMovies(movies)
+        movieDao.upsertMovies(popularMovies)
 
         movieDao.getPopularMoviesForHomeScreen().test {
             val result = awaitItem()
             assert(result.size == 3)
+            cancel()
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun getPopularMoviesForHomeScreen_returns_movies_in_correct_order() = runTest {
+        val popularMovies = movies.map {
+            it.copy(isPopular = true)
+        }
+
+        movieDao.upsertMovies(popularMovies)
+
+        movieDao.getPopularMoviesForHomeScreen().test {
+            val result = awaitItem()
+            assert(result[0].id == 2)
+            assert(result[1].id == 1)
+            assert(result[2].id == 3)
+            cancel()
+        }
+    }
+
+    @Test
+    fun getPopularMoviesForHomeScreen_returns_no_more_than_20_items() = runTest {
+        val popularMovies = createMovieListOfSpecifiedSite(50, isPopular = true)
+
+        movieDao.upsertMovies(popularMovies)
+
+        movieDao.getPopularMoviesForHomeScreen().test {
+            val result = awaitItem()
+            assert(result.isNotEmpty() && result.size <= 20)
+            cancel()
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun clearAll_clears_all_movies() = runTest {
+        val size = 30
+        val popularMovies = createMovieListOfSpecifiedSite(size = size, isPopular = true)
+
+        movieDao.upsertMovies(popularMovies)
+
+        movieDao.getPopularMoviesForHomeScreen().test {
+            val result = awaitItem()
+            assert(result.size == 20)
+            cancel()
+        }
+        movieDao.clearAll()
+        movieDao.getPopularMoviesForHomeScreen().test {
+            val result = awaitItem()
+            assert(result.isEmpty())
             cancel()
         }
     }
