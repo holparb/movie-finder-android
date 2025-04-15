c3369a0e73ec94bc9cf6a7789a504037e32c2230
Balazs Holpar <balazs02.holpar@gmail.com>
2025-04-15 09:05:26 +0200

    Set light icon colors for status and navigation bar

--- app/src/main/java/com/holparb/moviefinder/MainActivity.kt
+++ app/src/main/java/com/holparb/moviefinder/MainActivity.kt
@@ -2,8 +2,11 @@ package com.holparb.moviefinder
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import com.holparb.moviefinder.movies.presentation.main_screen.MainScreen
 import com.holparb.moviefinder.ui.theme.MovieFinderTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,7 +16,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.Transparent.toArgb()),
+            navigationBarStyle = SystemBarStyle.dark(Color.Transparent.toArgb())
+        )
         setContent {
             MovieFinderTheme {
                 MainScreen()
